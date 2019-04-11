2a684b11ad461f9690204981434e3bbc575cd7fc
Saptaparna10 <saptaparna1991@gmail.com>
2019-04-11 15:23:07 -0400

    services ijmplemented

---
+++ src/main/java/edu/northeastern/cs5610/services/RecipeService.java
@@ -0,0 +1,10 @@
+package edu.northeastern.cs5610.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.northeastern.cs5610.repositories.*;
+
+public class RecipeService {
+	@Autowired
+	RecipeRepository repository;
+}
---
+++ src/main/java/edu/northeastern/cs5610/services/RegisteredUserService.java
@@ -0,0 +1,12 @@
+package edu.northeastern.cs5610.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.northeastern.cs5610.repositories.*;
+
+public class RegisteredUserService {
+
+	@Autowired
+	RegisteredUserRepository repository;
+	
+}
