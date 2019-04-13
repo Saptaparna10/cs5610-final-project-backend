6bd1a8205afa69e59cfe394be46d18a6763f7319
Saptaparna10 <saptaparna1991@gmail.com>
2019-04-12 23:06:44 -0400

    deployed to heroku

--- src/main/java/edu/northeastern/cs5610/services/UserService.java
+++ src/main/java/edu/northeastern/cs5610/services/UserService.java
@@ -6,14 +6,18 @@ import java.util.Optional;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import edu.northeastern.cs5610.models.User;
 import edu.northeastern.cs5610.repositories.*;
 
+@RestController
+@CrossOrigin(origins = "http://localhost:4200/", allowCredentials="true") 
 public class UserService {
 
 	@Autowired
