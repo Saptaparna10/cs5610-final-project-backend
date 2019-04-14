25ddef8ac97443a2e890bdd694276c92447f15f6
Saptaparna10 <saptaparna1991@gmail.com>
2019-04-14 15:43:34 -0400

    favorite service implemented

--- src/main/java/edu/northeastern/cs5610/repositories/FavouriteRepository.java
+++ src/main/java/edu/northeastern/cs5610/repositories/FavouriteRepository.java
@@ -1,9 +1,17 @@
 package edu.northeastern.cs5610.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import edu.northeastern.cs5610.models.*;
 
 public interface FavouriteRepository extends CrudRepository<Favourite, Integer>{
+	
+	@Query("SELECT r FROM Favourite r WHERE r.favrecipe=:recipe")
+	Iterable<Favourite> findFavoritesForRecipe(@Param("recipe") Recipe recipe);
+	
+	@Query("SELECT r FROM Favourite r WHERE r.user=:user")
+	Iterable<Favourite> findFavoritesByUser(@Param("user") RegisteredUser user);
 
 }
--- src/main/java/edu/northeastern/cs5610/services/FavouriteService.java
+++ src/main/java/edu/northeastern/cs5610/services/FavouriteService.java
@@ -1,10 +1,55 @@
 package edu.northeastern.cs5610.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import edu.northeastern.cs5610.models.Favourite;
+import edu.northeastern.cs5610.models.Recipe;
+import edu.northeastern.cs5610.models.RegisteredUser;
 import edu.northeastern.cs5610.repositories.*;
 
 public class FavouriteService {
 	@Autowired
 	FavouriteRepository repository;
+	
+	@Autowired
+	RegisteredUserService regUserService;
+	
+	@Autowired
+	RecipeService recipeService;
+	
+	@PostMapping("/api/favorite")
+	public Favourite createFavourite(@RequestBody Favourite fav) {
+		return repository.save(fav);
+	}
+	
+	@GetMapping("/api/favorite/recipe/{id}")
+	public Iterable<Favourite> findFavoritesForRecipe(@PathVariable("id") int id) {
+		Recipe recipe= recipeService.findRecipeById(id);
+		Iterable<Favourite> favs=  repository.findFavoritesForRecipe(recipe);
+		if(favs != null) {
+			return favs;
+		}
+		else {
+			return null;
+		}
+	}
+	
+	@GetMapping("/api/favorite/user/{id}")
+	public Iterable<Favourite> findFavoritesByUser(@PathVariable("id") int id) {
+		RegisteredUser user= regUserService.findRegisteredUserById(id);
+
+		Iterable<Favourite>  favs=  repository.findFavoritesByUser(user);
+		if(favs != null) {
+			return favs;
+		}
+		else {
+			return null;
+		}
+	}
 }
