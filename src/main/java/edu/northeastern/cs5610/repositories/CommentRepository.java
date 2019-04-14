691668c339910dc180cb383e56bed87c336a749f
Saptaparna10 <saptaparna1991@gmail.com>
2019-04-14 16:07:43 -0400

    comment service implemented

--- src/main/java/edu/northeastern/cs5610/repositories/CommentRepository.java
+++ src/main/java/edu/northeastern/cs5610/repositories/CommentRepository.java
@@ -1,9 +1,21 @@
 package edu.northeastern.cs5610.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import edu.northeastern.cs5610.models.Comment;
+import edu.northeastern.cs5610.models.Recipe;
+import edu.northeastern.cs5610.models.RegisteredUser;
 
 public interface CommentRepository extends CrudRepository<Comment, Integer>{
 
+
+	@Query("SELECT r FROM Comment r WHERE r.commentingUser=:user and r.recipe=:recipe")
+	Iterable<Comment> findCommentsByUserForRecipe(@Param("user") RegisteredUser user, @Param("recipe") Recipe recipe);
+
+	@Query("SELECT r FROM Comment r WHERE r.recipe=:recipe")
+	Iterable<Comment> findAllCommentsForSong(@Param("recipe") Recipe recipe);
 }
--- src/main/java/edu/northeastern/cs5610/services/CommentService.java
+++ src/main/java/edu/northeastern/cs5610/services/CommentService.java
@@ -1,11 +1,93 @@
 package edu.northeastern.cs5610.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import edu.northeastern.cs5610.models.*;
 import edu.northeastern.cs5610.repositories.*;
 
 public class CommentService {
 	
 	@Autowired
 	CommentRepository repository;
+	
+	@Autowired
+	RegisteredUserService regUserService;
+	
+	@Autowired
+	RecipeService recipeService;
+	
+	@PostMapping("/api/comment/user/{userId}/recipe/{recipeId}")
+	public Comment createComment(@PathVariable("userId") int userId, @PathVariable("recipeId") int recipeId, @RequestBody Comment comment) {
+		RegisteredUser user = regUserService.findRegisteredUserById(userId);
+		Recipe recipe = recipeService.findRecipeById(recipeId);
+		
+		comment.setUser(user);
+		comment.setRecipe(recipe);
+		
+		comment = repository.save(comment);
+		recipe.getCommentsReceived().add(comment);
+		
+		recipeService.updateRecipe(recipeId, recipe);
+		regUserService.updateRegisteredUser(userId, user);
+		return comment;
+	}
+	
+	@GetMapping("/api/comment")
+	public List<Comment> findAllComments(){
+		return (List<Comment>) repository.findAll();
+	}
+	
+	@GetMapping("/api/comment/{id}")
+	public Comment findCommentById(@PathVariable("id") int id) {
+		
+		return repository.findById(id).get();
+	}
+	
+	@GetMapping("/api/comment/user/{userId}/recipe/{recipeId}")
+	public List<Comment> findCommentsByUserForRecipe(@PathVariable("userId") int userId, @PathVariable("recipeId") int recipeId) {
+		
+		return (List<Comment>) repository.findCommentsByUserForRecipe(regUserService.findRegisteredUserById(userId), recipeService.findRecipeById(recipeId));
+	}
+	
+	@GetMapping("/api/comment/recipe/{recipeId}")
+	public List<Comment> findAllCommentsForSong(@PathVariable("recipeId") int recipeId){
+		
+		return (List<Comment>) repository.findAllCommentsForSong(recipeService.findRecipeById(recipeId));
+	}
+	
+	@PutMapping("/api/comment/{id}")
+	public Comment updateComment(@PathVariable("id") int id, @RequestBody Comment comment) {
+		Comment prev = findCommentById(id);
+		prev.set(comment);
+		return repository.save(prev);
+	}
+	
+	@DeleteMapping("/api/comment/{id}")
+	public void deleteComment(@PathVariable("id") int id) {
+		
+		Comment comment = findCommentById(id);
+		comment.getCommentingUser().getComments().remove(comment);
+		comment.getRecipe().getCommentsReceived().remove(comment);
+		
+		recipeService.updateRecipe(comment.getRecipe().getId(), comment.getRecipe());
+		regUserService.updateRegisteredUser(comment.getCommentingUser().getId(),comment.getCommentingUser());
+
+		repository.deleteById(id);
+	}
+	
+	@DeleteMapping("/api/comment")
+	public void deleteAllComments() {
+		List<Comment> comments = findAllComments();
+		for(Comment comment: comments) {
+			deleteComment(comment.getId());
+		}
+	}
 }
