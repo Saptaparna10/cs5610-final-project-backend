45fd3fa4fd14ae7010e379342482ff4dae6fd10d
Saptaparna10 <saptaparna1991@gmail.com>
2019-04-15 20:27:25 -0400

    implemented undo fsvorite

--- src/main/java/edu/northeastern/cs5610/services/FavouriteService.java
+++ src/main/java/edu/northeastern/cs5610/services/FavouriteService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,6 +44,24 @@ public class FavouriteService {
 		recipeService.updateRecipe(recipeId, recipe);
 		regUserService.updateRegisteredUser(userId, user);
 		return fav;
+	}
+	
+	@DeleteMapping("/api/favorite/{id}")
+	public void deleteFavorite(@PathVariable("id") int id) {
+		
+		Favourite fav = findFavoritetById(id);
+		fav.getUser().getFavourites().remove(fav);
+		fav.getFavrecipe().getCommentsReceived().remove(fav);
+		
+		recipeService.updateRecipe(fav.getFavrecipe().getId(), fav.getFavrecipe());
+		regUserService.updateRegisteredUser(fav.getUser().getId(),fav.getUser());
+
+		repository.deleteById(id);
+	}
+	
+	@GetMapping("/api/favorite/{id}")
+	public Favourite findFavoritetById(@PathVariable("id") int id) {
+		return repository.findById(id).get();
 	}
 	
 	@GetMapping("/api/favorite/recipe/{id}")
