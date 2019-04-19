a497a8d006eff4399b96670b3c8f9e956e42a2a9
Saptaparna10 <saptaparna1991@gmail.com>
2019-04-18 23:55:09 -0400

    modified addRecipeToRecipeList

--- src/main/java/edu/northeastern/cs5610/services/RecipeListService.java
+++ src/main/java/edu/northeastern/cs5610/services/RecipeListService.java
@@ -19,33 +19,33 @@ import edu.northeastern.cs5610.repositories.RecipeListRepository;
 @RestController
 @CrossOrigin(origins = "*", allowCredentials = "true", allowedHeaders = "*")
 public class RecipeListService {
-	
+
 	@Autowired
 	private RecipeListRepository repository;
-	
+
 	@Autowired
 	private RecipeService recipeService;
-	
+
 	@Autowired
 	private ModeratorService modService;
-	
+
 
 	@PostMapping("/api/recipelist/{modId}")
 	public RecipeList createRecipeList(@PathVariable("modId") int modId, @RequestBody RecipeList recipeList) {
 		recipeList = repository.save(recipeList);
 
 		Moderator owner = modService.findModeratorById(modId);
-				
+
 		if(owner == null)
 			return null;
-		
+
 		if(owner != null) {
 			recipeList.setModerator(owner);
 			owner.getRecipeLists().add(recipeList);
 			modService.updateModerator(owner.getId(), owner);
 		}
 		return recipeList;
-		
+
 	}
 
 	@GetMapping("/api/recipelist/{id}")
@@ -63,7 +63,7 @@ public class RecipeListService {
 	public List<RecipeList> findAllRecipeLists(){
 		return (List<RecipeList>) repository.findAll();
 	}
-	
+
 	@GetMapping("/api/moderator/{mid}/recipe/{rid}")
 	public Boolean searchModeratorRecipeList(@PathVariable("mid") int moderatorId, @PathVariable("rid") String recipeId){
 		List<RecipeList> recipeList = (List<RecipeList>)repository.findRecipeListByModerator(modService.findModeratorById(moderatorId));
@@ -77,36 +77,38 @@ public class RecipeListService {
 		}
 		return false;
 	}
-	
+
 	@GetMapping("/api/moderator/{mid}/recipelist")
 	public List<RecipeList> searchRecipeListByModerator(@PathVariable("mid") int moderatorId){
 		List<RecipeList> recipeList = (List<RecipeList>)repository.findRecipeListByModerator(modService.findModeratorById(moderatorId));
 		return recipeList;
 	}
-	
-	
+
+
 	@PutMapping("/api/recipelist/{recipelistId}/recipe/{recipeId}")
 	public RecipeList addRecipeToRecipeList(@PathVariable("recipelistId") int recipelistId, @PathVariable("recipeId") String recipeId) {
-		
+
 		RecipeList recipelist = findRecipeListById(recipelistId);
 		Recipe recipe = recipeService.findRecipeById(recipeId);
-		
+
 		if(recipelist != null && recipe != null) {
-			recipelist.addRecipeToRecipeList(recipe);
-			return repository.save(recipelist);
+			if(!recipelist.getRecipes().contains(recipe)) {
+				recipelist.addRecipeToRecipeList(recipe);
+				return repository.save(recipelist);
+			}
 		}
 		System.out.println("Either recipeList or recipe is NULL");
 		return null;
-		
+
 	}
-	
+
 
 	@DeleteMapping("/api/recipelist/{recipelistId}/recipe/{recipeId}")
 	public RecipeList removeRecipeFromRecipeList(@PathVariable("recipelistId") int recipelistId, @PathVariable("recipeId") String recipeId) {
-		
+
 		RecipeList recipeList = findRecipeListById(recipelistId);
 		Recipe recipe = recipeService.findRecipeById(recipeId);
-		
+
 		if(recipeList != null && recipe != null) {
 			recipeList.removeRecipeFromRecipeList(recipe);
 			return repository.save(recipeList);
@@ -114,7 +116,7 @@ public class RecipeListService {
 		System.out.println("Either recipeList or recipe is NULL");
 		return null;
 	}
-	
+
 
 	@PutMapping("/api/recipelist/{id}")
 	public RecipeList updateRecipeList(@PathVariable("id") int id, @RequestBody RecipeList recipeList) {
@@ -124,7 +126,7 @@ public class RecipeListService {
 		prevlist.set(recipeList);
 		return repository.save(prevlist);
 	}
-	
+
 
 	@GetMapping("/api/recipelist/name/{name}")
 	public RecipeList findRecipListByName(@PathVariable("name") String name) {
@@ -134,7 +136,7 @@ public class RecipeListService {
 		}
 		return null;
 	}
-	
+
 
 	@DeleteMapping("/api/recipelist/{id}")
 	public void deleteRecipeList(@PathVariable ("id") int id) {
@@ -151,10 +153,10 @@ public class RecipeListService {
 			owner.getRecipeLists().remove(recipelist);
 			modService.updateModerator(owner.getId(), owner);
 		}
-		
+
 		repository.deleteById(id);
 	}
-	
+
 
 	@DeleteMapping("/api/recipelist")
 	public void deleteAllRecipeLists() {
@@ -163,13 +165,13 @@ public class RecipeListService {
 			deleteRecipeList(recipelist.getId());
 		}
 	}
-	
+
 	@GetMapping("/api/recipelist/{cid}/recipes")
 	public List<Recipe> getRecipesByRecipeList(@PathVariable ("cid") int cid) {
 		RecipeList list = findRecipeListById(cid);
 		if(list == null)
 			return null;
-		
+
 		return list.getRecipes();
 	}
 }
